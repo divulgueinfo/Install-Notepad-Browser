17aae91c3efac4d3947fa43475d76ae980ff3fe6
Daviny Vidal <daviny.vidal@outlook.com>
2020-09-06 12:35:31 -0300

    first commit

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 echo "Seu nome de usuário é:"
 whoami
 
-sudo apt install update
-sudo apt install upgrade
-sudo apt install apache2
-sudo apt install php
-
-cd /opt/
-mkdir Notepad-Browser
-cd Notepad-Browser
+sudo mkdir /opt/Notepad-Browser
+cd /opt/Notepad-Browser/
 sudo chmod 777 -R /opt/Notepad-Browser/
 git clone git@github.com:davinyvidal/Notepad-Browser.git
-sudo chmod 777 -R /opt/Notepad-Browser/all/
+cd Notepad-Browser
+sudo chmod 777 -R /opt/Notepad-Browser/Notepad-Browser/all/
 
 
 sudo cp file.notepad.conf.exemplo /etc/apache2/sites-available/file.notepad.conf
@@ -22,28 +17,27 @@ sudo cp link.notepad.conf.exemplo /etc/apache2/sites-available/link.notepad.conf
 
 if grep -r 'Notepad-Browser' /etc/apache2/apache2.conf
   then echo "[ok]"
-  else echo "escrevendo no arquivo"
-fi
-
-echo "<Directory /opt/Notepad-Browser/>" >> "/etc/apache2/apache2.conf"
-echo "        Options Indexes FollowSymLinks" >> "/etc/apache2/apache2.conf"
-echo "        AllowOverride None" >> "/etc/apache2/apache2.conf"
-echo "        Require all granted" >> "/etc/apache2/apache2.conf"
-echo "</Directory>" >> "/etc/apache2/apache2.conf"
-
-
-if grep -r 'create.notepad' /etc/hosts
-  then echo "[ok]"
-  else echo "127.0.0.1   create.notepad" >> "/etc/hosts"
+  else
+	 echo "escrevendo no arquivo"
+	 echo "<Directory /opt/Notepad-Browser/>" >> "/etc/apache2/apache2.conf"
+	 echo "        Options Indexes FollowSymLinks" >> "/etc/apache2/apache2.conf"
+	 echo "        AllowOverride None" >> "/etc/apache2/apache2.conf"
+         echo "        Require all granted" >> "/etc/apache2/apache2.conf"
+	 echo "</Directory>" >> "/etc/apache2/apache2.conf"
 fi
 
 if grep -r 'create.notepad' /etc/hosts
   then echo "[ok]"
-  else echo "127.0.0.1   link.notepad" >> "/etc/hosts"
+  else  echo "127.0.0.1   create.notepad" >> "/etc/hosts"
+fi
+
+if grep -r 'link.notepad' /etc/hosts
+  then echo "[ok]"
+  else  echo "127.0.0.1   link.notepad" >> "/etc/hosts"
 fi
 
 
-if grep -r 'create.notepad' /etc/hosts
+if grep -r 'file.notepad' /etc/hosts
   then echo "[ok]"
   else echo "127.0.0.1   file.notepad" >> "/etc/hosts"
 fi
@@ -61,7 +55,7 @@ sudo service apache2 restart
 echo "Link simbolico"
 echo ""
 
-ln -s /opt/Notepad-Browser/all/  ~/Files
+ln -s /opt/Notepad-Browser/Notepad-Browser/all/  ~/Files
 
 echo "Fim"
 echo ""
